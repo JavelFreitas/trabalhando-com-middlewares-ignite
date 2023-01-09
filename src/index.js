8de6dcfa9a977ea4d076f3d376fc7d3d00bb184f
Javel Queiroz <javelqueiroz@outlook.com.br>
2023-01-08 21:42:18 -0300

    feat: middlewares for user and todo verification

--- src/index.js
+++ src/index.js
@@ -22,15 +22,48 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
-}
+  const { user } = request;
+  if(user.pro !== true && user.todos.length >= 10){
+    response.status(403).json({error: 'Unable to create todo'});
+  }
 
+  return next();
+}
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  if(!user){
+    return response.status(404).json({error: 'User not found'});
+  }
+
+  if(!validate(id)){
+    return response.status(400).json({error: 'Id not valid'});
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if(!todo){
+    return response.status(404).json({error: 'Todo not found'});
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  if(!user){
+    return response.status(404).json({error: 'User not found'});
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
